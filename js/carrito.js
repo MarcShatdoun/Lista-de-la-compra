844c637959bfdd88bb40991112bc8d0176bf051c
Marc Quimis <marc.quimis@gmail.com>
2024-03-11 13:56:03 +0100

    Crear lista

---
+++ js/carrito.js
@@ -0,0 +1,65 @@
+/*
+Hay que programar un carrito de compra de fruta.
+
+El cliente eligirá que fruta quiere haciendo click sobre la imagen.
+Un mensaje emergente le preguntará qué cantidad quiere.
+
+Esta información se mostrará a la derecha, bajo "Total carrito", 
+en <p id="carrito"></p>, de esta forma:
+ Kiwi 2 kg x 4,20€/kg = 8,40 €
+
+El total se actualizará con cada compra
+ Total Compra: 8,40€
+ 
+Se dará la opción de añadir o no más productos que se mostrarán
+a continuación de los anteriores, y se sumará todo en el total. 
+Por ejemplo:  
+ Kiwi 2 kg x 4, 20€/kg = 8, 40€
+ Pomelo 1 kg x 2,50€/kg = 2,50€
+ Total Compra: 10,90€
+
+Puedes modificar el código facilitado si ello te ayuda con el ejercicio,
+pero deberás justificarlo.
+
+Recuerda la importancia comentar con detalle el código.
+
+ Lo importante es el cálculo, no los estilos css
+ */
+
+// const valorFruta = document.getElementsByName("fruta")
+
+// valorFruta.addEventListener("click", () => {
+
+//     alert('hola')
+// })
+
+// function pomelo(){
+//     let kilosFrutas = prompt(`¿Qué cantidad de pomelo desea?`)
+
+//     let valor1 = 2.5
+//     let valorFinal = valor1 * kilosFrutas
+    
+// }
+
+let listaCompra = document.getElementById('carrito')
+let precioTotal = document.getElementById('preuFinal').textContent
+
+console.log(precioTotal);
+
+
+function fruta (nombre, valor) {
+    
+    
+    let cantidadFrutas = prompt(`¿Qué cantidad de ${nombre} desea?`)
+    let precioFinal = valor * cantidadFrutas
+
+    //tomamos valor del campo
+   
+    let valor = `${nombre} ${cantidadFrutas} x ${valor}/Kg = ${precioFinal}€` 
+    
+    
+
+    
+}
+
+
